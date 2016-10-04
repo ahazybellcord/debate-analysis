487ee1b82ed9cf2f5e8a4f850df339c613446632
ZL <zdlebold@buffalo.edu>
2016-10-04 00:50:21 -0400

    lot o' changes... currently implementing token separation for use with nltk Text

--- debate_analysis_z.py
+++ debate_analysis_z.py
@@ -6,102 +6,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from collections import Counter
+from nltk.book import Text
+#from nltk.book import Text
 
 special_words = ['I', 'Lester', 'Holt', 'Hillary', 'Clinton', 'Donald', 'Trump', 'Mr', 'Secretary', 'OK', 'ISIS',
                      'China', 'Sidney', 'Blumenthal', 'Republicans', 'NAFTA', 'Iran', 'Sean']
 
+capitalized_words = ["I", "I'd", "I'll", "I'm", "I've", "Mr.", "Donald", "Trump", "Lester", "Secretary", "Hillary",
+                     "OK", "China", "Iran", "NAFTA", "Republicans"]
+
 def main():
 
-    holt_lines, clinton_lines, trump_lines = parse_transcript()
+    words = get_words_from_file('debate_transcript2.txt')
 
-    '''
-    for line in holt_lines:
-        # print(line)
-        # match a location or proper name
-        matchObj = re.search(r'([A-Z][a-z]+, )?([A-Z][a-z]+ )+[A-Z][a-z]+', line)
-        if matchObj:
-            print("Found a location or name: ", matchObj.group())
-        matchObj = re.search(r'[A-Z][a-z]+ ([A-Z]\. )?[A-Z][a-z]+', line)
-        if matchObj:
-            print("Found a name: ", matchObj.group())
-    '''
+    holt_words, clinton_words, trump_words = partition_words(words, "HOLT","CLINTON","TRUMP")
 
-    for line in trump_lines:
-        matchObj = re.search(r'worst.*ever', line)
-        if matchObj:
-            print(line + '\n')
+    separate_nonword_tokens(holt_words)
+    #remove_nonwords(clinton_words)
+    #remove_nonwords(trump_words)
 
-    #exit(0)
+    #decapitalize(holt_words)
+    #decapitalize(clinton_words)
+    #decapitalize(trump_words)
 
-    holt_words = get_words_from_lines(holt_lines)
-    clinton_words = get_words_from_lines(clinton_lines)
-    trump_words =  get_words_from_lines(trump_lines)
+    trump_text = Text(trump_words)
 
-    trump_superlative_count = 0
-    trump_me_count = 0
-    trump_black_and_white_count = 0
 
-    superlatives = ['very', 'many', 'really', 'great', 'huge', 'extremely', 'tremendous', 'unbelievable', 'big', 'too']
-    black_and_white = ['good', 'bad']
-
-    # find consecutive words
-    for i in range(len(trump_words) - 1):
-        current = trump_words[i]
-        if current in superlatives:
-            trump_superlative_count += 1
-        if current in black_and_white:
-            trump_black_and_white_count += 1
-        if 'I' in current or current == 'me':
-            trump_me_count += 1
-
-    print('TRUMP: ')
-    trump_superlative_percentage = 100 * trump_superlative_count / len(trump_words)
-    print('Superlative percentage: ', trump_superlative_percentage)
-    trump_me_percentage = 100 * trump_me_count / len(trump_words)
-    print('Me percentage: ', trump_me_percentage)
-    trump_black_and_white_percentage = 100 * trump_black_and_white_count / len(trump_words)
-    print('Good/bad percentage: ', trump_black_and_white_percentage)
+    exit(0)
 
     lowercase_letters = [chr(c) for c in range(ord('a'), ord('z') + 1)]
     uppercase_letters = [chr(c) for c in range(ord('A'), ord('Z') + 1)]
-
-    # remove non-words
-    for word in trump_words:
-        for c in word:
-            if c not in lowercase_letters and c not in uppercase_letters:
-                if len(word) == 1:
-                    trump_words.remove(word)
-
-    for word in clinton_words:
-        for c in word:
-            if c not in lowercase_letters and c not in uppercase_letters:
-                if len(word) == 1:
-                    clinton_words.remove(word)
-
-    clinton_superlative_count = 0
-    clinton_me_count = 0
-    clinton_black_and_white_count = 0
-
-    # find consecutive words
-    for i in range(len(clinton_words) - 1):
-        current = clinton_words[i]
-        if current in superlatives:
-            clinton_superlative_count += 1
-        if current in black_and_white:
-            clinton_black_and_white_count += 1
-        if 'I' in current or current == 'me':
-            clinton_me_count += 1
-
-    print('CLINTON: ')
-    clinton_superlative_percentage = 100 * clinton_superlative_count / len(clinton_words)
-    print('Superlative percentage: ', clinton_superlative_percentage)
-    clinton_me_percentage = 100 * clinton_me_count / len(clinton_words)
-    print('Me percentage: ', clinton_me_percentage)
-    clinton_black_and_white_percentage = 100 * clinton_black_and_white_count / len(clinton_words)
-    print('Good/bad percentage: ', clinton_black_and_white_percentage)
-
-    print('Trump has ' + str(len(trump_words)) + ' words')
-    print('Clinton has ' + str(len(clinton_words)) + ' words')
 
     trump_dictionary = {}
     clinton_dictionary = {}
@@ -128,9 +62,8 @@ def main():
 
     filtered_tuples = [item for item in common_tuples if item[0] > 50]
 
-    print(filtered_tuples)
-
     interesting_words = ['they','very','because','what','think','would', 'be', 'great', 'bad', 'worst','look']
+
     for word in interesting_words:
         print(word.upper())
         print('Trump frequency: ' + str(trump_dictionary[word]))
@@ -138,7 +71,6 @@ def main():
         print('Clinton frequency: ' + str(clinton_dictionary[word]))
         print('Clinton percentage: ' + str(round(100 * clinton_dictionary[word] / len(clinton_words), 2)) + '%')
 
-    print(clinton_dictionary)
     create_frequent_histogram(trump_dictionary, 50, 'Most frequent words used by Trump in first debate','r')
 
     create_frequent_histogram(clinton_dictionary, 37, 'Most frequent words used by Clinton in first debate','b')
@@ -186,6 +118,9 @@ def main():
     #print(' '.join(cutup))
 
 def parse_transcript():
+
+
+
     holt_lines, clinton_lines, trump_lines = [], [], []
 
     holt = False
@@ -217,40 +152,72 @@ def parse_transcript():
                         print('Got line: ' + line)
     return holt_lines, clinton_lines, trump_lines
 
+def get_words_from_file(filename):
+    # read file replacing newlines with space
+    with open(filename) as f:
+        words = f.read().replace('\n', ' ')
 
-def get_words_from_lines(lines):
-    global special_words
+    # split text into list of words
+    words = words.split(' ')
 
-    all_words = []
-    for line in lines:
-        words = line.split(' ')
-        for word in words:
-            if not word:
-                words.remove(word)
-        for i in range(len(words)):
-            # found word with trailing punctuation
-            if '.' in words[i] or '?' in words[i]:
-                # check if next word is proper noun. if not, make lowercase
-                if i + 1 < len(words):
-                    next_word = words[i + 1]
-                    if 'A' <= next_word[0] <= 'Z' and re.sub(r'[.,?"]', '', next_word) not in special_words:
-                        # print('Next word after punctuation is: ' + next_word)
-                        # print('Changed to: ' + words[i+1])
-                        words[i + 1] = words[i + 1].lower()
-            words[i] = re.sub(r'[.;,?"]', '', words[i])
-        for word in words:
-            if not word:
-                words.remove(word)
-                # if word[0] >= 'A' and word[0] <= 'Z' and word not in special_words:
-                #  print('Found capitalized word: ' + word)
-        first_word = words[0]
-        if 'A' <= first_word[0] <= 'Z' and first_word not in special_words:
-            words[0] = words[0].lower()
-        for word in words:
-          all_words.append(word)
-    return all_words
+    # remove empty words
+    for word in words:
+        if not word:
+            words.remove(word)
+
+    return words
+
+# split text into each speaker's words
+# the last three arguments are tokens delineating each
+# speaker in the original text (e.g. "HOLT", "CLINTON", "TRUMP")
+def partition_words(text, moderator, candidateA, candidateB):
+    moderator_words = []
+    candidateA_words = []
+    candidateB_words = []
+
+    # assume text begins with moderator
+    current = moderator_words
+
+    # switch when encountering new speaker token
+    # otherwise append to current speaker's words
+    for i in range(len(text)):
+        if moderator in text[i]:
+            current = moderator_words
+        elif candidateA in text[i]:
+            current = candidateA_words
+        elif candidateB in text[i]:
+            current = candidateB_words
+        else:
+            current.append(text[i])
+
+    return moderator_words, candidateA_words, candidateB_words
+
+def separate_nonword_tokens(words):
+    for word in words:
+        matchObj = re.search(r'\W*',word)
+        if matchObj:
+            print(word)
 
 
+
+
+def decapitalize(words):
+    global capitalized_words
+
+    for i in range(len(words)):
+        # look for ends of sentences
+        matchObj = re.search(r'.*[.?!]',words[i])
+        c = ''
+        if '.' in words[i]:
+            c = '.'
+        elif '?' in words[i]:
+            c = '?'
+        elif '!' in words[i]:
+            c = '!'
+        if matchObj and i + 1 < len(words):
+            print(c + ' ' + words[i+1])
+            if words[i+1] not in capitalized_words:
+                words[i+1] = words[i+1].lower()
 
 def create_frequent_histogram(dictionary, min_occurrences, title, color):
 
